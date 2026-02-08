5a944ace60b041c26f3462a983bc52df778d47eb
topcasinoz <junetdo2022@gmail.com>
2026-02-09 00:05:19 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -86,13 +86,14 @@ if(form){
   bd.style.display='flex';
   function closeGate(){ bd.style.display='none'; bd.remove(); }
   bd.querySelector('#age-yes').addEventListener('click', function(){
-    window.location.href = "https://p8r9.com/?utm_campaign=RdPi6v684H&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+    window.location.href = "https://inf4hub.com/?utm_campaign=zxSxVTCG6M&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
   });
                                                 
   bd.querySelector('#age-no').addEventListener('click', function(){
-    window.location.href = "https://p8r9.com/?utm_campaign=RdPi6v684H&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+    window.location.href = "https://inf4hub.com/?utm_campaign=zxSxVTCG6M&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
   });
 })();
+
 
 
 
